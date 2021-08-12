809dcd3b6a6e7acf2220c01327a2fab434eaa95a
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-12 11:10:00 +0500

    Data types: String and Nubers

--- week 1/script.js
+++ week 1/script.js
@@ -1,2 +1,6 @@
 let a  = "Hello Zufizar Abdumurodova";
+let firstName = "Zulfizar";
+let surname = "Abdumurodova";
+let age = 20;
 document.write(a);
+document.write(`<h1> ${firstName} ${surname} is ${age}</h1>`);
