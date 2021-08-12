116271c489590955164a0a4de6f258f49b8a5677
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-12 17:58:06 +0500

    /Create simple app with promp

--- week 1/script.js
+++ week 1/script.js
@@ -1,6 +1,15 @@
-let a  = "Hello Zufizar Abdumurodova";
-let firstName = "Zulfizar";
-let surname = "Abdumurodova";
-let age = 20;
-document.write(a);
-document.write(`<h1> ${firstName} ${surname} is ${age}</h1>`);
+let username = prompt("What is your name?");
+
+let myWork = new Array("Muslimaat.uz", "Najot Ta'lim", "Nest One")
+
+if (username === "Zulfizar") {
+  document.write(`<h1>Zulfizar, you will be good person and work at ${myWork[0]}, with this Allah will give you Jannat</h1>`);
+} else {
+  document.write(`<h1>Hello ${username}, welcome to our page!</h1>`);
+}
+
+var image = new Image();
+
+image.src = 'house.jpg'
+
+document.querySelector('body').appendChild(image);
