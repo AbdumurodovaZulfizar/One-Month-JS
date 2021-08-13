aa4efef8b18744d63647db7cec95d3e4764b819b
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-13 16:33:26 +0500

    Create random game function

--- week 1/script.js
+++ week 1/script.js
@@ -4,14 +4,36 @@ let myWork = new Array("Muslimaat.uz", "Najot Ta'lim", "Nest One")
 
 if (username === "Zulfizar") {
   document.write(`<h1>Zulfizar, you will be good person and work at ${myWork[0]}, with this Allah will give you Jannat</h1>`);
-} else if(username === "Chris") {
+  var myimage = new Image();
+
+  myimage.src = 'house.jpg'
+
+  document.querySelector('body').appendChild(myimage);
+} else if (username === "Chris") {
   document.write(`<h1>Your dream come true, ${username}!!! You won BMW</h1>`);
+  randomize();
 } else {
   document.write(`<h1>Hello ${username}, welcome to our page!</h1>`);
+  randomize();
 }
 
-var image = new Image();
+function randomize() {
+  let carNames = ['Cobalt', 'Malibu', 'Tracker'];
 
-image.src = 'house.jpg'
+  let cobalt  = new Image();
+  cobalt.src = './cobalt.jpg';
+  let malibu = new Image();
+  malibu.src = './malibu.jpg';
+  let tracker = new Image();
+  tracker.src = './tracker.jpg';
 
-document.querySelector('body').appendChild(image);
+  let carImages = [cobalt, malibu, tracker];
+
+
+  var randomNumber  = Math.floor(Math.random() * carNames.length);
+  let CurrentCar = document.createElement('h4');
+  CurrentCar.innerText = carNames[randomNumber];
+  CurrentImage = carImages[randomNumber];
+  document.querySelector('body').appendChild(CurrentCar);
+  document.querySelector('body').appendChild(CurrentImage);
+}
