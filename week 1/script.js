aea6f92e845cbe46cac81b41222aa567f1ac8c54
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-13 16:10:13 +0500

    Add Chris to winners

--- week 1/script.js
+++ week 1/script.js
@@ -2,8 +2,10 @@ let username = prompt("What is your name?");
 
 let myWork = new Array("Muslimaat.uz", "Najot Ta'lim", "Nest One")
 
-if (username === "Zulfizar" || username === "Chris") {
+if (username === "Zulfizar") {
   document.write(`<h1>Zulfizar, you will be good person and work at ${myWork[0]}, with this Allah will give you Jannat</h1>`);
+} else if(username === "Chris") {
+  document.write(`<h1>Your dream come true, ${username}!!! You won BMW</h1>`);
 } else {
   document.write(`<h1>Hello ${username}, welcome to our page!</h1>`);
 }
