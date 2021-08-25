410c7fb2af7d94453bcaa27f05871be847b46fef
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-25 16:22:58 +0500

    Create function that multiples number properies

--- object exercises/script.js
+++ object exercises/script.js
@@ -14,10 +14,24 @@ function sumOfProperties(obj) {
   return sum;
 }
 
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
+function multiplyNumeric(obj) {
+  for( let key in obj) {
+    if (Number(obj[key]) === obj[key]) {
+      obj[key] *= 2
+    }
+    else {
+      obj[key] = obj[key];
+    }
+  }
+  return obj;
 }
 
-console.log(sumOfProperties(salaries));
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+
+console.log(multiplyNumeric(menu));
+
+// after the call
