c7afe3adf33e0ad1ea3ae0c988ad0e7afefc85dd
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-25 16:17:52 +0500

    Create function for sums properties

--- object exercises/script.js
+++ object exercises/script.js
@@ -5,3 +5,19 @@ function isEpty(obj) {
   }
   return array.length == 0;
 }
+
+function sumOfProperties(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key];
+  }
+  return sum;
+}
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+console.log(sumOfProperties(salaries));
